7db97efa880c8c610baa788890c4e4733a1fe79f
Misaka10049 <87744356+Misaka10049@users.noreply.github.com>
2023-04-16 10:30:54 +0800

    Add files via upload

--- player.js
+++ player.js
@@ -5,12 +5,17 @@ var playing=0
 var audio=new Audio()
 audio.preload="auto"
 audio.src="Audio/"+list[playing]
+document.body.append(audio)
+const button_text=()=>{
+	if(!audio.paused) $("play_song").innerText="暂停"
+	else $("play_song").innerText="播放"
+}
 const song_name=()=>{
 	$("name").innerText=list[playing].slice(0,-4)
 }
 const play_song=()=>{
-	if(!audio.paused) audio.pause(),$("play_song").innerText="播放"
-	else audio.play(),$("play_song").innerText="暂停"
+	if(!audio.paused) audio.pause()
+	else audio.play()
 }
 const last_song=()=>{
 	playing--
@@ -20,10 +25,11 @@ const last_song=()=>{
 }
 const next_song=()=>{
 	playing++
-	if(playing==list.length-1) playing=0
+	if(playing==list.length) playing=0
 	audio.src="Audio/"+list[playing]
 	play_song(),song_name()
 }
 $("play_song").addEventListener("click",play_song)
 $("last_song").addEventListener("click",last_song)
 $("next_song").addEventListener("click",next_song)
+setInterval(button_text,25)
